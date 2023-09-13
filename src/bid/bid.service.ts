d9b0c58c91430b2ed20d9d859e3ff1d6a47be2d2
Imat Marasigan <raymart.marasigan@gmail.com>
2023-09-13 20:01:28 +0800

    bug fix

--- src/bid/bid.service.ts
+++ src/bid/bid.service.ts
@@ -1,10 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { Bid } from './entity/bid.entity';
 import { EntityManager, Repository } from 'typeorm';
 import { User } from 'src/auth/entity/user.entity';
 import { Item, Status } from './entity/items.entity';
-import { CurrentUser } from 'src/auth/strategy/current-user.decorator';
+
 
 @Injectable()
 export class BidService {
@@ -18,12 +18,13 @@ export class BidService {
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
 
+    @InjectEntityManager() private readonly entityManager: EntityManager,
   ) {}
 
   public async placeBid(item: Item, user: User, bidAmount: number) {
 
     const currentBid = await this.bidRepo.findOne({
-      where: { user: user },
+      where: { user: user, item: item },
     });
 
     await this.itemRepo.save(
@@ -35,32 +36,21 @@ export class BidService {
     );
 
     await this.updateUserBalance(user, bidAmount);
-    
-    if (currentBid) {
-      return await this.bidRepo.update(
-        { id: currentBid.id },
-        { bidAmount: bidAmount },
-      );
 
-    } else {
-      return await this.bidRepo.save(
-        new Bid({
-          user: user,
-          item: item,
-          bidAmount: bidAmount,
-        }),
-      );
-    }
+    return await this.bidRepo.save({
+      ...currentBid,
+      user: user,
+      item: item,
+      bidAmount: bidAmount,
+    });
 
   }
 
   private async updateUserBalance(user: User, bidAmount: number) {
-    const updatedUser = await this.userRepo.save(
-      new User({
-        ...user,
-        balance: user.balance - bidAmount,
-      }),
-    );
+    const updatedUser = await this.userRepo.save({
+      ...user,
+      balance: user.balance - bidAmount,
+    });
     return updatedUser;
   }
 
